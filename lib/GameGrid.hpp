1293831133ebf56a156fd3e7f74b74c0d4b704ac
ll02842w <lucas.lescure@etu.univ-st-etienne.fr>
2024-03-14 16:14:50 +0000

    Grid class declaration reworked - missing documentation

--- lib/GameGrid.hpp
+++ lib/GameGrid.hpp
@@ -9,40 +9,41 @@
 /**
  * @class GameGrid
  * @brief 2D Grid built upon LetterBlocks that shape the playing field for the game
+ *
  */
 class GameGrid : sf::NonCopyable {
 private:
-    u_int8_t rows = 15;
     u_int8_t cols = 10;
+    u_int8_t rows = 15;
 
     std::vector<std::vector<LetterBlock>> grid = std::vector<std::vector<LetterBlock>>(rows, std::vector<LetterBlock>(cols));
 
 public:
-    GameGrid();
-    GameGrid(u_int8_t rows, u_int8_t cols);
+    GameGrid(u_int8_t cols = 10, u_int8_t rows = 15) : cols(cols), rows(rows) {}
 
     /**
      * @brief Updates the grid to make corresponding LetterBlocks fall 1 block lower, when possible
      */
-    void gridTick(void);
+    void gridTick();
 
     /**
      * @brief Destroys LetterBlocks that form a word
-     * @param span Coordinates and spanning of the word staged to delete
+     * @param span Coordinates and spanning of the word staged for destruction
      */
     void gridDestroy(sf::Rect<int> span);
 
     /**
      * @brief Updates the visibility respective LetterBlock from grid
-     * @param block The block to be hidden
+     * @param coord The coordinates of the block to be hidden
      */
-    void gridDispay(LetterBlock block, bool hide);
+    void gridDispay(sf::Vector2u coord, bool visible);
 
     /**
      * @brief Updates the visibility a zone of blocks delimited by the span entered
      * @param span The span of blocks to be hidden
      */
-    void gridDisplay(sf::Rect<int> span, bool hide);
+    void gridDisplay(sf::Rect<u_int8_t> span, bool visible);
+
 };
 
 #endif
