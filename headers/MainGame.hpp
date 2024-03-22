ffd574a6255904eb6dca46fb96ab7af07192d4ec
ll02842w <lucas.lescure@etu.univ-st-etienne.fr>
2024-03-22 16:25:15 +0100

    Test instructions for preliminary design construction

--- headers/MainGame.hpp
+++ headers/MainGame.hpp
@@ -3,19 +3,12 @@
 #define MAIN_GAME_HPP
 
 #include <SFML/Graphics.hpp>
+#include "GameDisplay.hpp"
 
 class MainGame : sf::NonCopyable {
 public:
-    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(1920,1080),"Lettris");
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(960,540),"Lettris");
 
-    MainGame() {
-        while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event))
-                if (event.type == sf::Event::Closed) 
-                    window.close();
-        }
-    }
 };
 
 #endif
