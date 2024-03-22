d7998d8ea89732d578a02ea6b62bcf2ac11d0735
ll02842w <lucas.lescure@etu.univ-st-etienne.fr>
2024-03-22 12:28:42 +0100

    Centralised game components to MainGame class for legibility

--- headers/MainGame.hpp
+++ headers/MainGame.hpp
@@ -7,6 +7,15 @@
 class MainGame : sf::NonCopyable {
 public:
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(1920,1080),"Lettris");
+
+    MainGame() {
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event))
+                if (event.type == sf::Event::Closed) 
+                    window.close();
+        }
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,30 +4,9 @@
 #include "headers/MainGame.hpp"
 
 int main(int argc, char const* argv[]) {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Hello From SFML");
 
-    // MainGame game;
-    // game.window = sf::RenderWindow(sf::VideoMode(1920,1080),"test");
+    MainGame game;
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Magenta);
-    LetterBlock test;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        window.clear();
-        window.draw(shape);
-        window.draw(test.getBlock());
-        window.display();
-        shape.move(sf::Vector2f(10, 10));
-        test.setColor(sf::Color::Red);
-        test.display(true);
-    }
 
     return 0;
 }
