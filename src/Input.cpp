5837615dd5d62e3890574c9e8a2367b3e105f3bf
Tom695 <tom.paillet999@gmail.com>
2024-03-29 15:52:10 +0100

    Added documentation

--- src/Input.cpp
+++ src/Input.cpp
@@ -4,6 +4,7 @@
 
 void Input::pollEvent() {
     direction = Direction::Unchanged;
+    // Directions respectfully stored from input
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) direction = Down;
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) direction = Left;
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) direction = Right;
