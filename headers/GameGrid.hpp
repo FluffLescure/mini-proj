4c4df4f75edeef3508aefa5fcc70924153e806b8
ll02842w <lucas.lescure@etu.univ-st-etienne.fr>
2024-03-30 18:38:43 +0100

    Added full documentation

--- headers/GameGrid.hpp
+++ headers/GameGrid.hpp
@@ -14,56 +14,108 @@ typedef std::vector<std::vector<LetterBlock>> Blockgrid;
 
 /**
  * @class GameGrid
- * @brief 2D Grid built upon LetterBlocks that shape the playing field for the game
+ * 
+ * @brief 2D grid built upon LetterBlocks that shape the playing field
+ * for the game. 
+ * 
+ * @remark This is where most of the game is controlled.
+ * 
+ * The dimensions of the grid are pulled from Config for scalablity. 
+ * 
+ * Agregation with Input and GameWordle are used to retrieve user input and 
+ * determine which block - that form a valid word - must be drestroyed.
+ * 
+ * 'tick' will be a counter that allows control of the game grid at any chosen
+ * tick.
+ * 
+ * 
+ * 
  */
 class GameGrid : sf::NonCopyable {
 private:
+
+    // dimensions of the grid;
     uint8_t cols;
     uint8_t rows;
 
+    // Border used to draw the grid frame
     sf::RectangleShape gridBorder;
 
+    // Agregated classes
     Input *input;
     GameWordle *wordle;
 
+    // The 2D grid where the game takes place
     Blockgrid grid;
 
+    // Game tick counter
     uint8_t tick = 0;
 
 public:
+    /**
+     * @brief Constructor which calls the initialisation of components and
+     * agregated classes
+    */
     GameGrid();
 
     /**
-     * @brief Updates the grid to make corresponding LetterBlocks fall 1 block lower, when possible
+     * @brief Updates the grid to make necessary LetterBlocks fall 1 block lower.
      */
     void gridTick();
 
+    /**
+     * @brief Updates the falling blocks based on user input
+    */
     void blockTick();
 
     /**
-     * @brief Destroys LetterBlocks that form a word
-     * @param span Coordinates and spanning of the word staged for destruction
+     * @brief Destroys LetterBlocks that are inside the specified span
+     * @param posInit the starting position of the span
+     * @param span span of blocks to be destroyed
      */
     void blockDestroy(sf::Vector2u posInit, sf::Vector2u span);
 
     /**
      * @brief Updates the visibility respective LetterBlock from grid
      * @param coord The coordinates of the block to be hidden
+     * @param visible sets the the visibility of the block
      */
     void blockDisplay(sf::Vector2u coord, bool visible);
 
     /**
      * @brief Updates the visibility a zone of blocks delimited by the span entered
-     * @param span The span of blocks to be hidden
+     * @param posInit the starting position of the span
+     * @param span span of blocks to be updated
+     * @param visible sets the the visibility of the block
      */
     void blockDisplay(sf::Vector2u posInit, sf::Vector2u span, bool visible);
 
+
+    /**
+     * @brief Builds the 2D grid using dimensions pulled from Config
+     * as well as the grid's bounding frame
+    */
     void initGrid();
 
+    /**
+     * @brief Renders the grid's bounding frame and the 2D game grid
+     * @param *target the rendered shared to other classes
+    */
     void render(sf::RenderTarget *target);
 
+    /**
+     * @brief Actively retrieves user input and updates the game correspondingly.
+     * Every 5 ticks the blocks are updated in relation to the input, and every
+     * 30 ticks the grid is updated based on the state of the grid at that point.
+    */
     void update();
 
+    /**
+     * @brief Checks if a block should be grounded based on game rules and sets
+     * them grounded if needed.
+     * @param i the column of the block
+     * @param j the row of the block
+    */
     void groundBlock(uint8_t i, uint8_t j);
 
 };
