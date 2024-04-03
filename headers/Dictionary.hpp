db7097eeb1b273b46b0e8aaad085da19456f9307
ll02842w <lucas.lescure@etu.univ-st-etienne.fr>
2024-04-03 17:02:02 +0200

    Added hashmap for keyword identification from dictionary, add init of hash and renamed files

---
+++ headers/Dictionary.hpp
@@ -0,0 +1,34 @@
+#ifndef GAME_WORDLE_HPP
+#define GAME_WORDLE_HPP
+
+#include<string>
+#include<vector>
+#include<unordered_map>
+
+#include "LetterBlock.hpp"
+
+/** This class' only prupose is to check if a valid word is contained inside the grid, be it horizontally or vertically.
+ * 
+ * The algorithm used to check if a word is formed will be using a hashmap
+ * 
+ * The class should not contain a GameGrid member, nor should it inherit the class. 
+ * 
+ * No method will have more than 3 layers of nesting, and each method will serve a unique function. 
+ */
+class Dictionary {
+
+private:
+    std::vector<std::string> dictionary;
+    std::unordered_map<std::string, int> hashmap;
+public:
+    Dictionary();
+
+    void initDictionnary();
+    void initHashmap();
+    sf::Vector2u foundHash(const std::string& word);
+
+
+
+};
+
+#endif
