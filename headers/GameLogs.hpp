32ec9b89eaccc009dd9b30b65f7aaa2f7373b4fa
Tom695 <tom.paillet999@gmail.com>
2024-04-04 09:27:49 +0200

    Created Logs

---
+++ headers/GameLogs.hpp
@@ -0,0 +1,42 @@
+#ifndef GAME_LOGS_HPP
+#define GAME_LOGS_HPP
+
+
+
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+
+#include "LetterBlock.hpp"
+
+
+/**
+ * @class GameLogs
+ * 
+ * @brief Logs showing the previously made words and their attributed scores
+ * 
+ * 
+ * 
+ * 
+ */
+class GameLogs : sf::NonCopyable {
+private:
+
+    // Border used to draw the grid frame
+    sf::RectangleShape gridBorder;
+
+
+public:
+    GameLogs();
+
+    /**
+     * @brief Renders the grid's bounding frame and the 2D game grid
+     * @param *target the rendered shared to other classes
+    */
+    void render(sf::RenderTarget *target);
+
+};
+
+#endif
