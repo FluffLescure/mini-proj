e0e7e669a16ef95eb3fa07bef6f52d2f0b0cb882
Tom695 <tom.paillet999@gmail.com>
2024-04-05 16:28:25 +0200

    Added history

--- src/GameLogs.cpp
+++ src/GameLogs.cpp
@@ -1,4 +1,5 @@
 #include "../headers/GameLogs.hpp"
+#include "../headers/Config.hpp"
 
 
 GameLogs::GameLogs(){
@@ -6,10 +7,32 @@ GameLogs::GameLogs(){
     gridBorder.setPosition(sf::Vector2f(53, 162));
     gridBorder.setOutlineColor(sf::Color(125, 125, 125));
     gridBorder.setOutlineThickness(5);
-    gridBorder.setFillColor(sf::Color(0, 0, 0, 0));
+    gridBorder.setFillColor(sf::Color(0, 0, 0, 0)); 
+
+    title.setString("Stats");
+    title.setCharacterSize(30);
+    title.setFillColor(sf::Color::White);
+    title.setFont(*(Config::getInstance()->font));
+    title.setOrigin(sf::Vector2f(title.getGlobalBounds().getSize().x / 2.f + title.getLocalBounds().getPosition().x,0));
+    title.setPosition(sf::Vector2f(158,162));
+
+    sf::Vector2f tsize;
+
+    for (int i=0;i<10;i++){
+        historique[i].setString("Random");
+        historique[i].setCharacterSize(18);
+        historique[i].setFillColor(sf::Color::White);
+        historique[i].setFont(*(Config::getInstance()->font));
+        tsize = sf::Vector2f(historique[i].getGlobalBounds().getSize() + historique[i].getLocalBounds().getPosition()); //size of the text
+        historique[i].setPosition(sf::Vector2f(63,220+i*(tsize.y+5)));
+    }
 }
 
 
 void GameLogs::render(sf::RenderTarget *target){
     target->draw(gridBorder); // renders the frame
+    target->draw(title);
+    for(int i = 0; i<10; i++){
+        target->draw(historique[i]);
+    }
 }
