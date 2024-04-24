e05939a0ba79faf8d760ae31904a79f49522ee7c
FluffLescure <Lucas.Lescure1@gmail.com>
2024-04-24 20:17:24 +0200

    cleaning and documentation

--- headers/Wordle.hpp
+++ headers/Wordle.hpp
@@ -2,23 +2,15 @@
 #define GAME_WORDLE_HPP
 
 #include<string>
+#include<map>
 
 #include <SFML/System/Vector2.hpp>
 
 
-struct foundWord{
-    std::string word;
-    sf::Vector2i position;
-};
-
-
-/** This class' only prupose is to check if a valid word is contained inside the grid, be it horizontally or vertically.
- * 
- * The algorithm used to check if a word is formed will be using a hashmap
- * 
- * The class should not contain a GameGrid member, nor should it inherit the class. 
- * 
- * No method will have more than 3 layers of nesting, and each method will serve a unique function. 
+/**
+ * @class Woordle
+ * @brief Provides word detection of a combination of characters
+ * based on a word map created from a wordlist.
  */
 class Wordle {
 
